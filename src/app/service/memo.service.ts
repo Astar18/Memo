4025a2661eb9313c2511698d9d6031c332784a37
Astar18 <62781005+Astar18@users.noreply.github.com>
2024-05-29 23:12:20 -0500

    Update memo.service.ts

--- src/app/service/memo.service.ts
+++ src/app/service/memo.service.ts
@@ -1,9 +1,36 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { MemoComponent } from '../components/memo/memo.component';
+import { Observable } from 'rxjs';
+import { Memo } from '../components/memo/memo.component';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class MemoService {
+  private url = 'assets/memo.json';
+  constructor(private http: HttpClient) { }
+  getMemos(): Observable<Memo[]> {
+    return this.http.get<Memo[]>(this.url);
+  }
 
-  constructor() { }
+  crearMemo(memo: Memo): Observable<Memo> {
+    // Aquí deberías implementar la lógica para añadir el memo al archivo JSON
+    return this.http.post<Memo>(this.url, memo);
+  }
+
+  actualizarMemo(memo: Memo): Observable<Memo> {
+    // Aquí deberías implementar la lógica para actualizar el memo en el archivo JSON
+    return this.http.put<Memo>(`${this.url}/${memo.id}`, memo);
+  }
+
+
+  eliminarMemo(id: number): Observable<void> {
+    // Aquí deberías implementar la lógica para eliminar el memo del archivo JSON
+    return this.http.delete<void>(`${this.url}/${id}`);
+  }
+
+
+
 }
